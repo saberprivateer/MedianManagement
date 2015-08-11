be7a51d7377697ebd56f337cc2fc2ecefa532f3b
saberprivateer <daniel.kamerling@gmail.com>
2015-08-11 00:10:20 -0700

    werks

--- src/Test/medmanage/MainTest.java
+++ src/Test/medmanage/MainTest.java
@@ -4,75 +4,83 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static medmanage.Main.log;
+
 public class MainTest {
 
     @Test
     public void testInsertMax() throws Exception {
-        Main main = new Main();
-//        String[] args = new String[0];
-        main.grabFile("Sample");
+        Main.grabFile("Sample");
         int next;
-        for (int i = 0;i<main.n;i++){
-            next = Integer.parseInt(main.argInput[i]);
-            main.insertMax(next);
+        for (int i = 0; i < Main.n; i++) {
+            next = Integer.parseInt(Main.argInput[i]);
+            Main.insertMax(next);
         }
         Assert.assertTrue(true);
-        int max = main.lowHeap.get(0);
-        int num6 = main.lowHeap.get(5);
+        int max = Main.lowHeap.get(0);
+        int num6 = Main.lowHeap.get(5);
         Assert.assertEquals("The max item should be 90", 90, max);
         Assert.assertEquals("Item 6 should be 7", 7, num6);
     }
 
     @Test
     public void testGrabFile() throws Exception {
-        Main main = new Main();
-        main.grabFile("Sample");
-        Assert.assertEquals("File length should be 10", 10, main.n);
-        Assert.assertEquals("Second entry should be 7", "7", main.argInput[1]);
+        Main.grabFile("Sample");
+        Assert.assertEquals("File length should be 10", 10, Main.n);
+        Assert.assertEquals("Second entry should be 7", "7", Main.argInput[1]);
     }
 
-//    @Test
-//    public void testInsertMax2() throws Exception {
-////        Main main = new Main();
-////        String[] args = new String[0];
-//        Main.grabFile("Sample");
-//        int next;
-//        for (int i = 0;i<Main.n;i++){
-//            next = Integer.parseInt(Main.argInput[i]);
-//            Main.insertMax(next);
-//        }
-//        Assert.assertTrue(true);
-//        int max = Main.lowHeap.get(0);
-//        int num6 = Main.lowHeap.get(5);
-//        Assert.assertEquals("The max item should be 90", 90, max);
-//        Assert.assertEquals("Item 6 should be 7", 7, num6);
-//    }
+    @Test
+    public void testParse() throws Exception {
+        Main.grabFile("Sample");
+        Main.parse();
+        Assert.assertTrue(true);
+        Assert.assertEquals("The rolling sum is 122 at the end", 122, Main.sumMedians);
+    }
+
+    @Test
+    public void testExtractMin() throws Exception {
+        Main.grabFile("Sample");
+        int next;
+        for (int i = 0; i < Main.n; i++) {
+            next = Integer.parseInt(Main.argInput[i]);
+            Main.insertMin(next);
+        }
+//        log("Before extracting:");
+//        Main.printHigh();
+        Main.extractMin();
+        Assert.assertTrue(true);
+        int check = Main.highHeap.get(2);
+//        log("I extracted: " + Main.hold);
+//        Main.printHigh();
+        Assert.assertEquals("The first extracted value should be 1", 1, Main.hold);
+        Assert.assertEquals("The third entry is 17", 17, check);
+    }
 
     @Before
     public void setUp() throws Exception {
-
-
+        System.gc();
+        Main.reset();
     }
 
+    //
     @After
     public void tearDown() throws Exception {
-
-
+        System.gc();
     }
 
     @Test
     public void testInsertMin() throws Exception {
-        Main main = new Main();
-        String[] args = new String[0];
-        main.grabFile("Sample");
+        Main.grabFile("Sample");
         int next;
-        for (int i = 0;i<main.n;i++){
-            next = Integer.parseInt(main.argInput[i]);
-            main.insertMin(next);
+        for (int i = 0; i < Main.n; i++) {
+            next = Integer.parseInt(Main.argInput[i]);
+            Main.insertMin(next);
         }
         Assert.assertTrue(true);
-        int max = main.highHeap.get(0);
-        int num2 = main.highHeap.get(1);
+        int max = Main.highHeap.get(0);
+        int num2 = Main.highHeap.get(1);
         Assert.assertEquals("The min item should be 1", 1, max);
         Assert.assertEquals("Item 2 should be 3", 3, num2);
     }
--- src/com/medmanage/Main.java
+++ src/com/medmanage/Main.java
@@ -19,6 +19,22 @@ public class Main {
         System.out.println(args.toString());
     }
 
+    public static void reset() {
+        lowHeap = new ArrayList<>();
+        highHeap = new ArrayList<>();
+        sumMedians = 0;
+    }
+
+    public static void printHigh() {
+        log("The highHeap array:");
+        System.out.print("[");
+        for (int i = 0; i < Main.highHeap.size(); i++) {
+            System.out.print(Main.highHeap.get(i) + " ");
+        }
+        System.out.print("]");
+        log("");
+    }
+
     public static void insertMax(int arg) {
         lowHeap.add(arg);
         int index = lowHeap.size() - 1;
@@ -83,8 +99,8 @@ public class Main {
 
     public static void extractMin() {
         hold = highHeap.get(0);
-        int child1index = 1;
-        int child2index = 2;
+        int child1index;
+        int child2index;
         int child1 = 0;
         int child2 = 0;
         boolean child1exists;
@@ -92,27 +108,27 @@ public class Main {
         int index = 0;
         boolean notHeaped = true;
         while (notHeaped) {
-
             //Are we at the last #?
             if (index >= highHeap.size()) {
                 notHeaped = false;
             }
-
+            child1index = index * 2 + 1;
+            child2index = index * 2 + 2;
             //Compare to the level below
             if (notHeaped) {
                 child1exists = false;
                 child2exists = false;
                 if (child2index < highHeap.size()) {
                     child2 = highHeap.get(child2index);
-                    child1exists = true;
+                    child2exists = true;
                 }
                 if (child1index < highHeap.size()) {
-                    child2exists = true;
+                    child1exists = true;
                     child1 = highHeap.get(child1index);
                 }
 
                 //First condition: both children exist
-                if (child2exists & child1exists) {
+                if (child2exists && child1exists) {
                     if (child1 < child2) {
                         highHeap.set(index, child1);
                         index = child1index;
@@ -122,46 +138,180 @@ public class Main {
                     }
                 }
 
-                //Second condition: 
-                if (!child1exists & !child2exists) {
+                //Second condition: no children exist
+                if (!child1exists && !child2exists) {
+                    highHeap.set(index, highHeap.get(highHeap.size() - 1));
                     notHeaped = false;
                 }
 
+                //Third condition: first child exists
+                if (child1exists && !child2exists) {
+                    highHeap.set(index, child1);
+                    notHeaped = false;
+                }
             }
         }
+        highHeap.remove(highHeap.size() - 1);
+    }
+
+    public static void extractMax() {
+        hold = lowHeap.get(0);
+        int child1index;
+        int child2index;
+        int child1 = 0;
+        int child2 = 0;
+        boolean child1exists;
+        boolean child2exists;
+        int index = 0;
+        boolean notHeaped = true;
+        while (notHeaped) {
+
+            //Are we at the last #?
+            if (index >= lowHeap.size()) {
+                notHeaped = false;
+            }
+            child1index = index * 2 + 1;
+            child2index = index * 2 + 2;
+            //Compare to the level below
+            if (notHeaped) {
+                child1exists = false;
+                child2exists = false;
+                if (child2index < lowHeap.size()) {
+                    child2 = lowHeap.get(child2index);
+                    child2exists = true;
+                }
+                if (child1index < lowHeap.size()) {
+                    child1exists = true;
+                    child1 = lowHeap.get(child1index);
+                }
+
+                //First condition: both children exist
+                if (child2exists && child1exists) {
+                    if (child1 > child2) {
+                        lowHeap.set(index, child1);
+                        index = child1index;
+                    } else {
+                        lowHeap.set(index, child2);
+                        index = child2index;
+                    }
+                }
+
+                //Second condition: no children exist
+                if (!child1exists && !child2exists) {
+                    lowHeap.set(index, lowHeap.get(lowHeap.size() - 1));
+                    notHeaped = false;
+                }
+
+                //Third condition: first child exists
+                if (child1exists && !child2exists) {
+                    lowHeap.set(index, child1);
+                    notHeaped = false;
+                }
+            }
+        }
+        lowHeap.remove(lowHeap.size() - 1);
     }
 
     public static void parse() {
         int next;
+        //stream of numbers
         for (int i = 0; i < n; i++) {
             next = Integer.parseInt(argInput[i]);
-            insertMax(next);
-        }
-        for (int i = 0; i < n; i++) {
-            next = Integer.parseInt(argInput[i]);
-            insertMin(next);
-        }
+            //if empty, add it to the low heap
+            if (lowHeap.isEmpty()) {
+                insertMax(next);
+                sumMedians = next;
+            } else {
+                //if tiny add to the lowheap
+                if (next > lowHeap.get(0)) {
+                    insertMin(next);
+                } else {
+                    //if not tiny, add to the highHeap
+                    insertMax(next);
+                }
 
+//                log("The lowHeap array after switch:");
+//                for (int j = 0; j < lowHeap.size(); j++) {
+//                    System.out.print(lowHeap.get(j) + " ");
+//                }
+//                log("");
+//                log("The highHeap array after switch:");
+//
+//                for (int j = 0; j < highHeap.size(); j++) {
+//                    System.out.print(highHeap.get(j) + " ");
+//                }
+//                log("");
+
+                //check for balance
+//                log("The case will be " + (lowHeap.size() - highHeap.size()) + ".");
+                switch (lowHeap.size() - highHeap.size()) {
+                    case 0:
+                        sumMedians += lowHeap.get(0);
+                        break;
+                    case 1:
+                        sumMedians += lowHeap.get(0);
+                        break;
+                    case -1:
+                        sumMedians += highHeap.get(0);
+                        break;
+                    case 2:
+                        extractMax();
+                        insertMin(hold);
+                        sumMedians += lowHeap.get(0);
+                        break;
+                    case -2:
+                        extractMin();
+                        insertMax(hold);
+                        sumMedians += lowHeap.get(0);
+                        break;
+                    default:
+                        sumMedians += lowHeap.get(0);
+                        break;
+                }
+
+
+            }
+//            log("The sumMedians a the " + i + "th number is " + sumMedians + ".");
+        }
+        log("The sumMedians is " + sumMedians + ".");
     }
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         long startTime = System.nanoTime();
 
-//        log("Begin Program");
-//        log("End Program");
-//        grabFile("Sample");
-//        parse();
+        log("Begin Program");
+        log("End Program");
+        Main main = new Main();
+        grabFile("Median");
+        parse();
+//        log("The highHeap array after parsing:");
 //        for (int i=0;i<lowHeap.size();i++){
 //            System.out.print(lowHeap.get(i)+" ");
 //        }
 //        log("");
 //
-//        for (int i=0;i<lowHeap.size();i++){
+//        for (int i=0;i<highHeap.size();i++){
 //            System.out.print(highHeap.get(i)+" ");
 //        }
+//        log("");
+//        extractMin();
+//        log("I extracted: "+hold);
+//        log("The new highHeap array after extraction:");
+//        for (int i=0;i<highHeap.size();i++){
+//            System.out.print(highHeap.get(i)+" ");
+//        }
+//        log("");
+//        extractMin();
+//        log("I extracted: " + hold);
+//        log("The new highHeap array after extraction:");
+//        for (int i=0;i<highHeap.size();i++){
+//            System.out.print(highHeap.get(i)+" ");
+//        }
+
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
-//        log("Program ran for " + duration / 1000000 + " milliseconds");
+
+        log("Program ran for " + duration / 1000000 + " milliseconds");
     }
 
     public static void grabFile(String arg) throws IOException, URISyntaxException {
